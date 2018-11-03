42af6f0360c584fcdece4f917bcda2ae05327dc8
uranoni <uranoni777@gmail.com>
2018-11-04 05:36:52 +0800

    body parser & cors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/',(req,res,next)=>{
 });
 
 router.post('/',(req,res,next)=>{
+  const order = {
+    productId:req.body.productId,
+    quantity:req.body.quantity
+  };
   res.status(201).json({
-    message:"order were created"
+    message:"order were created",
+    order:order
   });
 });
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,14 @@ router.get('/',(req,res,next)=>{
 });
 
 router.post('/',(req,res,next)=>{
+  const product={
+    name:req.body.name,
+    price:req.body.price
+  }
+  
   res.status(200).json({
-    message:"handling post /products"
+    message:"handling post /products",
+    createProduct:product
   });
 })
 
